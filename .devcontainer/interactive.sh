428c2dfa6b327a20937d5e72c33a4f70ef17b176
Josh Spicer <josh@joshspicer.com>
2023-06-02 18:05:32 +0000

    commit example

---
+++ .devcontainer/interactive.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+BOLD="\033[1m"
+RESET="\033[0m"
+
+# Prompt the user for input and do something with it.
+echo -e "${BOLD}What is your favorite color?${RESET}"
+read -p ">> " colorInput
+
+echo -e
+echo -e "Your favorite color is ${BOLD}${colorInput}${RESET}.  Great choice!"
