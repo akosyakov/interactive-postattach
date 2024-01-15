bd31ec481f3040286d48067500007a0c5826da48
Anton Kosyakov <anton@gitpod.io>
2024-01-15 14:37:53 +0100

    Update interactive.sh

--- .devcontainer/interactive.sh
+++ .devcontainer/interactive.sh
@@ -3,6 +3,8 @@
 BOLD="\033[1m"
 RESET="\033[0m"
 
+echo "${GITHUB_USER}"
+
 # Prompt the user for input and do something with it.
 echo -e "${BOLD}What is your favorite color?${RESET}"
 read -p ">> " colorInput
